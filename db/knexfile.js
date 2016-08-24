c3b34366f6d0aff525f8aae7f622230f43ee7e12
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-08-24 14:37:27 +0100

    Remove hello-world occurancies

--- db/knexfile.js
+++ db/knexfile.js
@@ -4,13 +4,13 @@ module.exports = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_URL || {
-      database: 'hello_world',
+      database: 'feedback',
     },
   },
   test: {
     client: 'pg',
     connection: process.env.DATABASE_URL || {
-      database: 'hello_world_test',
+      database: 'feedback_test',
     },
   },
 
