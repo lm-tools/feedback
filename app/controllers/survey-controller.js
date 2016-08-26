53f04430275f63470048dc4e8f80bf2e089873c7
Nathan Cashmore <nathan.cashmore@ucds.email>
2016-08-26 13:37:07 +0100

    Add intervention type and reference to request url

--- app/controllers/survey-controller.js
+++ app/controllers/survey-controller.js
@@ -6,7 +6,11 @@ router.get('/', (req, res) => {
   res.render('survey', {});
 });
 
-router.post('/', (req, res, next) =>
+router.get('/:type/:ref', (req, res) => {
+  res.render('survey', { ref: req.params.ref, type: req.params.type });
+});
+
+router.post('/:type/:ref', (req, res, next) =>
   new AnswersModel({ survey: 'first-survey', data: req.body }).save()
     .then((answers) => res.send(answers.serialize()))
     .catch((err) => next(err))
--- test/integration/fill-survey.spec.js
+++ test/integration/fill-survey.spec.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const helper = require('./support/integrationSpecHelper');
 const surveyPage = helper.surveyPage;
 const dbHelper = helper.dbHelper;
@@ -5,20 +6,28 @@ const expect = require('chai').expect;
 const AnswersModel = require('../../app/models/answers-model');
 
 describe('Fill survey', () => {
-  const survey = { name: 'Sample name', surname: 'Sample surname' };
+  const theType = 'ryws';
+  const theRef = '12345';
 
-  beforeEach(() => dbHelper.cleanDb().then(() => surveyPage.visit()));
+  const expectedSurveyFields = { name: 'Sample name', surname: 'Sample surname' };
+  const expectedSurvey = _.merge({ ref: theRef, type: theType }, expectedSurveyFields);
 
-  it('should render all survey fields', () =>
-    expect(surveyPage.getFieldsNames()).to.eql(Object.keys(survey))
-  );
+  beforeEach(() => dbHelper.cleanDb().then(() =>
+    surveyPage.visit(theType, theRef)));
+
+  it('should render all survey fields', () => {
+    expect(surveyPage.getFieldsNames()).to.eql(Object.keys(expectedSurveyFields));
+  });
+
+  it('should populate all survey reference fields', () => {
+    expect(surveyPage.getParamValues()).to.eql([theRef, theType]);
+  });
 
   it('should save filled form to database', () => {
-    surveyPage.fill(survey);
+    surveyPage.fill(expectedSurveyFields);
     return surveyPage.submit().then(() =>
       AnswersModel.fetchAll().then((answersList) =>
-        expect(answersList.serialize()[0].data).to.eql(survey)
+        expect(answersList.serialize()[0].data).to.eql(expectedSurvey)
       ));
   });
 });
-
