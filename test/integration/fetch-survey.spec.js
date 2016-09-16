e4aa8a197527a74660261dbe18e2b2d6a42096c2
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2016-09-16 15:43:20 +0100

    Use db migration definition in fetch survey tests

--- test/integration/fetch-survey.spec.js
+++ test/integration/fetch-survey.spec.js
@@ -5,103 +5,51 @@ const fetchSurveyScript = path.join(__dirname, '..', '..', 'scripts/fetch-survey
 const AnswerModel = require('../../app/models/answers-model');
 const dbHelper = require('./support/dbHelper');
 const expect = require('chai').expect;
+const ewycdSurveyDefinition = require('../../db/migrations/20160908142125_ewycd-2-definition')
+  .definition;
 
-describe('Fetch survey script', () => {
-  describe('executing "scripts/fetch-survey.js ewycd-1"', () => {
-    const sampleAnswers = {
-      survey_id: 'ewycd-1',
-      ref: '123',
-      data: {
-        whyTypes: [
-          'whyTypesStartGoals',
-          'whyTypesBroadenGoals',
-          'whyTypesTransferableSkills',
-          'whyTypesUpdateCv',
-          'whyTypesSearchTerms',
-          'whyTypesOther',
-        ],
-        whyTypesOtherReason: 'asdf',
-        startGoalsHelped: 'no',
-        broadenGoalsHelped: 'yes',
-        transferableSkillsHelped: 'no',
-        updateCvHelped: 'yes',
-        searchTermsHelped: 'no',
-        otherHelped: 'yes',
-        claimantChange: 'no',
-        claimantFeedback: 'adsf',
-        agentFeedback: 'asdf',
-        rating: '2',
-      },
-    };
-    let result;
-    before(() =>
-      dbHelper.cleanDb()
-        .then(() => new AnswerModel(sampleAnswers).save()
-        ).then(() => {
-          result = childProcess.spawnSync(fetchSurveyScript, ['ewycd-1']);
-        })
-    );
-
-    it('should execute without error', () => {
-      expect(result.status).to.eql(0);
-    });
-
-    it('should return labels for all the questions', () => {
-      expect(JSON.parse(result.stdout)).to.have.property('labels').that.include.keys(
-        [
-          'whyTypes',
-          'whyTypesStartGoals',
-          'whyTypesBroadenGoals',
-          'whyTypesTransferableSkills',
-          'whyTypesUpdateCv',
-          'whyTypesSearchTerms',
-          'whyTypesOther',
-          'whyTypesOtherReason',
-          'yes',
-          'no',
-          'startGoalsHelped',
-          'broadenGoalsHelped',
-          'transferableSkillsHelped',
-          'updateCvHelped',
-          'searchTermsHelped',
-          'otherHelped',
-          'claimantChange',
-          'claimantFeedback',
-          'agentFeedback',
-          'rating',
-        ]);
-    });
-
-    it('should return predefined options', () => {
-      expect(JSON.parse(result.stdout)).to.have.property('options').that.eql(
-        {
-          'whyTypes': [
-            'whyTypesStartGoals',
-            'whyTypesBroadenGoals',
-            'whyTypesTransferableSkills',
-            'whyTypesUpdateCv',
-            'whyTypesSearchTerms',
-            'whyTypesOther',
-          ],
-          'startGoalsHelped': ['yes', 'no'],
-          'broadenGoalsHelped': ['yes', 'no'],
-          'transferableSkillsHelped': ['yes', 'no'],
-          'updateCvHelped': ['yes', 'no'],
-          'searchTermsHelped': ['yes', 'no'],
-          'otherHelped': ['yes', 'no'],
-          'claimantChange': ['yes', 'no'],
-          'agentFeedback': ['yes', 'no'],
-          'rating': ['5', '4', '3', '2', '1'],
-        }
+[
+  ewycdSurveyDefinition,
+].forEach(surveyDefinition => {
+  describe(`Fetch "${surveyDefinition.type}" survey`, () => {
+    describe(`executing "scripts/fetch-survey.js ${surveyDefinition.id}"`, () => {
+      const sampleAnswers = {
+        survey_id: surveyDefinition.id,
+        ref: surveyDefinition.type,
+        data: {
+          some: 'data',
+        },
+      };
+      let result;
+      before(() =>
+        dbHelper.cleanDb()
+          .then(() => new AnswerModel(sampleAnswers).save()
+          ).then(() => {
+            result = childProcess.spawnSync(fetchSurveyScript, [surveyDefinition.id]);
+          })
       );
-    });
 
-    it('should return answers', () => {
-      expect(JSON.parse(result.stdout)).to.have.property('answers').that.eql(
-        [
-          Object.assign({}, sampleAnswers.data, { refId: sampleAnswers.ref }),
-        ]
-      );
+      it('should execute without error', () => {
+        expect(result.status).to.eql(0);
+      });
+
+      it('should return labels for all the questions', () => {
+        expect(JSON.parse(result.stdout)).to.have.property('labels').that
+          .include.keys(Object.keys(surveyDefinition.definition.labels));
+      });
+
+      it('should return predefined options', () => {
+        expect(JSON.parse(result.stdout)).to.have.property('options')
+          .that.eql(surveyDefinition.definition.options);
+      });
+
+      it('should return answers', () => {
+        expect(JSON.parse(result.stdout)).to.have.property('answers').that.eql(
+          [
+            Object.assign({}, sampleAnswers.data, { refId: sampleAnswers.ref }),
+          ]
+        );
+      });
     });
   });
 });
